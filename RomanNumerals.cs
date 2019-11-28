a5c0a0af8a697207604206e584bc97b2bc33498b
spartangian <gian.abinadab@gmail.com>
2019-11-28 17:07:37 -0600

    Add project files.

---
+++ RomanNumerals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabicToRomanNumerals
+{
+    enum RomanNumerals
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        M = 1000
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabicToRomanNumerals
+{
+    public class TestClass
+    {
+        public int Convert(string param)
+        {
+            int translatedValue = 0;
+            int paramLength = param.Length;
+
+            for(int i =0; i < paramLength; i++)
+            {
+                if(i > 0)
+                {
+                    if(GetArabicValue(param[i]) > GetArabicValue(param[i - 1]))
+                    {
+                        translatedValue = translatedValue - GetArabicValue(param[i - 1]);
+                        translatedValue += GetArabicValue(param[i]) - GetArabicValue(param[i - 1]);
+                    }
+                    else
+                    {
+                        translatedValue += GetArabicValue(param[i]);
+                    }
+                }
+                else
+                {
+                    translatedValue += GetArabicValue(param[i]);
+                }
+                
+            }
+
+            return translatedValue;
+        }
+
+        private int GetArabicValue(char param)
+        {
+            int value = (int)Enum.Parse(typeof(RomanNumerals), param.ToString());
+            return value;
+        }
+    }
+}
